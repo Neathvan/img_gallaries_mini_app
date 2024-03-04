4be1925caa32cd56823fcf88bdf36298548d569e
VanNeath <vanneath883@gmail.com>
2024-03-04 18:51:45 +0700

    ** Update code in api.dart

--- lib/service/api.dart
+++ lib/service/api.dart
@@ -1,10 +1,20 @@
 import 'dart:developer';
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+// import 'package:get/get.dart' hide Response;
 import 'package:img_gallaries_mini_app/service/config.dart';
 
-abstract class WebUtil {
-  Dio createDio() {
-    var dio = Dio(BaseOptions(
+abstract class BaseApi {
+  late Dio _dio;
+
+  BaseApi() {
+    createDio();
+  }
+
+  /// enables you to make HTTP requests
+  /// create instance of dio with default key-value
+  createDio() {
+    _dio = Dio(BaseOptions(
       baseUrl: IGMConfig.baseUrl,
       headers: defaultHeader,
       receiveDataWhenStatusError: true,
@@ -12,16 +22,17 @@ abstract class WebUtil {
       receiveTimeout: const Duration(seconds: 60), // 60 seconds
     ));
 
-    ///
-    dio.interceptors.add(InterceptorsWrapper(
+    /// add handling of requet resp and hanle
+    _dio.interceptors.add(InterceptorsWrapper(
       onRequest: (options, handler) {
-        log('url :: ${options.uri}\nheaders :: ${options.headers}\ndata :: ${options.data}',
-            name: 'REQUEST ==>');
+        if (kDebugMode) {
+          print(
+              '[====================== REQUEST ====================== ]\n** url :: ${options.uri}\n** headers :: ${options.headers}\n** data :: ${options.data}');
+        }
         return handler.next(options);
       },
       onResponse: (response, handler) {
-        log('url :: ${response.realUri}\ndata :: ${response.data}',
-            name: 'RESPONSE ==>');
+        log('[====================== RESPONSE ====================== ]\n** url :: ${response.realUri}\n** data :: ${response.data}');
         return handler.next(response);
       },
       onError: (e, handler) {
@@ -30,7 +41,6 @@ abstract class WebUtil {
         return handler.next(e);
       },
     ));
-    return dio;
   }
 
   set defaultHeader(Map<String, dynamic> value) {}
@@ -41,12 +51,13 @@ abstract class WebUtil {
       };
 }
 
-class Api extends WebUtil {
+class Api extends BaseApi {
   /// handle dio error
   Response _getResponseError(DioException e) {
     DioException de = e;
     if (e.response == null) {
       return Response(
+        data: {},
         requestOptions: de.requestOptions,
         statusCode: -1,
         statusMessage: de.message,
@@ -57,7 +68,7 @@ class Api extends WebUtil {
 
   Future post(String path, {Map<String, dynamic>? params}) async {
     try {
-      var response = await createDio().post(path, data: params);
+      var response = await _dio.post(path, data: params);
       return response;
     } on DioException catch (e) {
       return _getResponseError(e);
@@ -67,8 +78,8 @@ class Api extends WebUtil {
   Future get(String path,
       {Map<String, dynamic>? params, Map<String, dynamic>? header}) async {
     try {
-      var response = await createDio()
-          .get(path, data: params, options: Options(headers: header));
+      var response =
+          await _dio.get(path, data: params, options: Options(headers: header));
       return response;
     } on DioException catch (e) {
       return _getResponseError(e);
